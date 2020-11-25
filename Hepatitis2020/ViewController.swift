df705d138ee41e8cb22094ed2ff1b85d899268e9
Bruno <bruno@brunos-macbook-pro.local>
2020-11-25 08:16:14 -0800

    Initial Commit

---
+++ Hepatitis2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hepatitis2020
+//
+//  Created by Bruno on 25/11/2020.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
