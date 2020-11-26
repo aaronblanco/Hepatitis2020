e30914a399de71a4d34e2aef64157aedffc0fa31
aaron <aaron@iMac-de-aaron.local>
2020-11-26 04:21:49 -0800

    Datos pacientes personales y clínicos. Inicio, con sigues.

---
+++ Hepatitis2020/PacienteClinicDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PacienteClinicDataViewController.swift
+//  Hepatitis2020
+//
+//  Created by aaron on 26/11/2020.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class PacienteClinicDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hepatitis2020/PacientePersonalDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PacientePersonalDataViewController.swift
+//  Hepatitis2020
+//
+//  Created by aaron on 26/11/2020.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class PacientePersonalDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
